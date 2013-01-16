0d0f931ebe40287d8726a7f0a9659e877c75f2ee
Flavio <->
2013-01-16 11:21:40 +0100

    Split invocation handler

--- src/it/fb/structs/bytebuffer/StructArrayByteBufferImpl.java
+++ src/it/fb/structs/bytebuffer/StructArrayByteBufferImpl.java
@@ -2,6 +2,7 @@ package it.fb.structs.bytebuffer;
 
 import it.fb.structs.StructArray;
 import it.fb.structs.StructPointer;
+import it.fb.structs.Structs;
 import static it.fb.structs.Structs.*;
 import it.fb.structs.internal.Parser;
 import it.fb.structs.internal.SField;
@@ -41,13 +42,15 @@ public final class StructArrayByteBufferImpl<T> implements StructArray<T> {
     @Override
     public T get(int index) {
         return (T) Proxy.newProxyInstance(structInterface.getClassLoader(), 
-                new Class[]{structInterface}, new SABBInvocationHandler(implementors, index));
+                new Class[]{structInterface}, 
+                new SABBDataInvocationHandler(implementors, data, structSize, null, index));
     }
 
     @Override
     public StructPointer<T> at(int index) {
         return (StructPointer<T>) Proxy.newProxyInstance(structInterface.getClassLoader(), 
-                new Class[]{structInterface, StructPointer.class}, new SABBInvocationHandler(implementors, index));
+                new Class[]{structInterface, StructPointer.class}, 
+                new SABBPointerInvocationHandler(this, implementors, data, structSize, null, index));
     }
 
     public static <T> StructArrayByteBufferImpl<T> create(Class<T> structInterface, int size) {
@@ -55,7 +58,7 @@ public final class StructArrayByteBufferImpl<T> implements StructArray<T> {
         OffsetVisitor ov = new OffsetVisitor(4);
         Map<Method, IProxyMethodImplementor> implementors = new HashMap<>();
         for (SField field : desc.getFields()) {
-            Integer fieldOffset = field.accept(ov);
+            int fieldOffset = field.accept(ov);
             implementors.put(field.getGetter(), field.accept(new ProxyGetterMethodVisitor(fieldOffset)));
             if (field.getSetter() != null) {
                 implementors.put(field.getSetter(), field.accept(new ProxySetterMethodVisitor(fieldOffset)));
@@ -64,15 +67,19 @@ public final class StructArrayByteBufferImpl<T> implements StructArray<T> {
         ByteBuffer data = ByteBuffer.allocate(ov.getSize() * size);
         return new StructArrayByteBufferImpl<>(structInterface, size, ov.getSize(), implementors, data);
     }
-    
-    private final class SABBInvocationHandler implements InvocationHandler {
 
-        private final Map<Method, IProxyMethodImplementor> implementors;
-        private int index;
+    private static class SABBDataInvocationHandler implements InvocationHandler {
+        protected final Map<Method, IProxyMethodImplementor> implementors;
+        protected final ByteBuffer data;
+        protected final int structSize;
+        protected final SABBDataInvocationHandler parentHandler;
+        protected int index;
 
-        private SABBInvocationHandler(
-                Map<Method, IProxyMethodImplementor> implementors, int index) {
+        public SABBDataInvocationHandler(Map<Method, IProxyMethodImplementor> implementors, ByteBuffer data, int structSize, SABBDataInvocationHandler parentHandler, int index) {
             this.implementors = implementors;
+            this.data = data;
+            this.structSize = structSize;
+            this.parentHandler = parentHandler;
             this.index = index;
         }
 
@@ -80,19 +87,39 @@ public final class StructArrayByteBufferImpl<T> implements StructArray<T> {
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             IProxyMethodImplementor implementor = implementors.get(method);
             if (implementor != null) {
-                return implementor.run(data, index * structSize, args);
+                return implementor.run(data, getBaseOffset(), args);
             } else {
-                if (method.equals(POINTER_AT_METHOD)) {
-                    index = (Integer) args[0];
-                    return proxy;
-                } else if (method.equals(POINTER_GET_METHOD)) {
-                    return proxy;
-                } else if (method.equals(POINTER_OWNER_METHOD)) {
-                    return StructArrayByteBufferImpl.this;
-                } else {
-                    throw new UnsupportedOperationException(method.toString());
-                }
+                throw new UnsupportedOperationException(method.toString());
             }
         }
+        
+        public int getBaseOffset() {
+            return (parentHandler == null ? index * structSize : parentHandler.getBaseOffset() + index * structSize);
+        }
+    }
+    
+    private static final class SABBPointerInvocationHandler extends SABBDataInvocationHandler {
+
+        private final StructArrayByteBufferImpl owner;
+
+        public SABBPointerInvocationHandler(StructArrayByteBufferImpl owner, Map<Method, IProxyMethodImplementor> implementors, ByteBuffer data, int structSize, SABBDataInvocationHandler parentHandler, int index) {
+            super(implementors, data, structSize, parentHandler, index);
+            this.owner = owner;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (method.equals(POINTER_AT_METHOD)) {
+                index = (Integer) args[0];
+                return proxy;
+            } else if (method.equals(POINTER_GET_METHOD)) {
+                return proxy;
+            } else if (method.equals(POINTER_OWNER_METHOD)) {
+                return owner;
+            } else {
+                return super.invoke(proxy, method, args);
+            }
+        }
+
     }
 }
